e44ec87f8fe3f30cbab3f4310693cfeed72b236d
dneirac <dneirac@unsa.edu.pe>
2021-09-21 18:10:57 -0500

    Agregando comnetarios del Ejercicio

--- EjerciciosClase2/Ejercicio1.java
+++ EjerciciosClase2/Ejercicio1.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Ejercicio1{
   public static void main (String[]args){
+    /*Ingreso de datos donde n es la cantida de datos y k es el valor del que deben ser multiplo
+     * Mostrar cuanto de las entradas son divisbles entre k*/
     Scanner scan = new Scanner(System.in);
     int n  = scan.nextInt(), k = scan.nextInt(), rpta = 0;
     for (int i = 0; i < n; i++) {
