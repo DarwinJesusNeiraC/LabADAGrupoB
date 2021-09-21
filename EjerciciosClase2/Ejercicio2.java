edb542c3404ca7744be45826055bbaa3498d6e94
dneirac <dneirac@unsa.edu.pe>
2021-09-20 23:17:04 -0500

    Agregando contador de tiempo, prueba con 100 datos

--- EjerciciosClase2/Ejercicio2.java
+++ EjerciciosClase2/Ejercicio2.java
@@ -1,12 +1,15 @@
 import java.util.*;
 public class Ejercicio2{
   public static void main (String[]args){
+    int[] arr = new int [100];
+    fillIn(arr);
+    long start = System.currentTimeMillis();
     Scanner scan = new Scanner(System.in);
     System.out.print("ingresa un dato: ");
     int t = scan.nextInt();
-    int[] arr = {1,4,8,9,11,15,7,12,13,6};
     System.out.println(search(arr, t));
-    
+    long end = System.currentTimeMillis();
+    System.out.println("Tiempo de ejecucion " + (end-start)*1.0e-3 + " segundos");
   }
 
   public static boolean search(int [] arr, int t){
@@ -16,6 +19,12 @@ public class Ejercicio2{
       }
     }
     return false;
+  }
+
+  public static void fillIn(int [] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = i; 
+    }
   }
 }
 
