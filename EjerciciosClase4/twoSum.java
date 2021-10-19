36185630ec3d4fa8873135e77633756b6b97dcb2
dneirac <dneirac@unsa.edu.pe>
2021-10-18 20:09:36 -0500

    Se complento el programa para la suma de 2 num igual a 10 y que sean distintos

---
+++ EjerciciosClase4/twoSum.java
@@ -0,0 +1,28 @@
+/*
+ * Autor: Darwin Jesus Neira Carrasco
+ * Correo: dneirac@unsa.edu.pe
+ * Descripcion: Encontrar 2 numeros que sumen 10 y sean distintos
+ * la complijidad del elemento es de O(n²)*/
+
+public class twoSum{
+  public static void main (String[]args){
+    int [] arr = {3,7,2,6};
+    int [] arr2 = {5,5,2,6};
+    int [] arr3 = {5,8,1,6};
+    System.out.println("arr : " + twoSum(arr));
+    System.out.println("arr2 : " + twoSum(arr2));
+    System.out.println("arr3 : " + twoSum(arr3));
+
+  }
+  public static boolean twoSum(int [] arr){
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr.length; j++) {
+        if((i != j) && ( arr[i] + arr[j] == 10 )){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
+
