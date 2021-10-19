bad08d55047d7492dbb2d46f1866327e45170420
dneirac <dneirac@unsa.edu.pe>
2021-10-18 19:53:25 -0500

    Culminacion de ejercion de suma, si el indice es par

---
+++ EjerciciosClase4/everyOther.java
@@ -0,0 +1,27 @@
+/*Alumno: Darwin Jesus Neira Carrasco
+ *Descripcion: Realizar un programa que identifique los indices pares y sume los valores del arreglo
+ indentificado con dicho indice con el resto de elemetos, El programa tiene una complejidad de O(n^2)*/
+
+public class everyOther{
+  public static void main (String[]args){
+    int [] array = {1, 2, 3, 6, 7};
+    for (int i : array) {
+      System.out.print(i + ",");
+    }    
+    System.out.println("\n");
+    everyOther(array);
+
+  }
+
+  public static void everyOther(int [] array) {
+    for(int i = 0; i < array.length; i++){
+      if(i % 2 == 0 ){
+        for (int j = 0; j < array.length; j++) {
+          System.out.println(array[i] + array[j]);
+        }
+        System.out.println("\n");
+      }
+    }
+  }
+}
+
