94efe89efac8496810a0fd2169d87f0d1ab7df9a
dneirac <dneirac@unsa.edu.pe>
2021-10-29 20:46:13 -0500

    agregando explicacion del tiempo computacional

--- EjerciciosClase5/pickResume.py
+++ EjerciciosClase5/pickResume.py
@@ -4,6 +4,11 @@
 @Descripcion: Reduccion de una cadena a un valor unico, mediante
 un proceso de separacion de sus elemetos
 
+tiempo computacional
+En cada ejecucion se va reduciendo a la mitad el tamaño del arreglo
+por lo tanto esto sigue la estructura de los logarimos en este caso como
+lo va diviendo entre 2 seria log base 2 de n debdio a que el while dura hasta que
+la longitud de resume sea mayor a 1; O(log2(n))
 """
 def pick_resume(resume):
     eliminate = "top"
