c84a69d11b8afdae9ef50904aba36961f2d0cbe7
dneirac <dneirac@unsa.edu.pe>
2021-11-10 11:07:53 -0500

    Agreganod comentarios del tiempo computacional

---
+++ EjerciciosClase5/QEjercicios/Q2.java
@@ -0,0 +1,16 @@
+/*
+ * @Autor: Darwin Jesus Neira Carrasco
+ * @Correo: dneirac@unsa.edu.pe
+ * @Descripcion: Complijidad del elemento es de O(n)
+*/
+public class Q2{
+  public static void main (String[]args){
+    int n = 10;
+    for (int i = n; i > 0; i--) { // max(3n, 1) = O(3n) = O(n)
+      System.out.println("Ejecicio2");
+    }
+    /*El total de sentencias que se repiten una cantidad de n veces
+     * es 3, y la unica que se declara una vez la del incio del ciclo*/
+  }
+}
+
