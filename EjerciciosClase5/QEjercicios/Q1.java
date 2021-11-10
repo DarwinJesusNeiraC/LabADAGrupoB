0b9a0d42aacf23deb1a00b8b2ce41234a3dc86f0
dneirac <dneirac@unsa.edu.pe>
2021-11-10 11:02:03 -0500

    Creacion de la carpeta QEjercicios

---
+++ EjerciciosClase5/QEjercicios/Q1.java
@@ -0,0 +1,14 @@
+/*
+ * @Autor: Darwin Jesus Neira Carrasco
+ * @Correo: dneirac@unsa.edu.pe
+ * @Descripcion: Complijidad del elemento es de O(n)
+*/
+public class Q1{
+  public static void main (String[]args){
+    int n = 10;
+    for (int i = 0; i < n; i++) { //max(3n, 1) = O(3n) = O(n)
+      System.out.println("Ejercicio1");
+    }
+  }
+}
+
