2d6f45469fd32c884f1b1d78e38a0bf45acf6514
dneirac <dneirac@unsa.edu.pe>
2021-11-10 15:47:45 -0500

    completando codigo y comentando

---
+++ EjerciciosClase5/QEjercicios/Q10.py
@@ -0,0 +1,14 @@
+"""
+@Alumno: Darwin Jesus Neira Carrasco
+@Correo: dneirac@unsa.edu.pe
+@Descripcion: Complejidad es de O(n)
+
+"""
+n = int(input("ingresa el valor de n: "))
+for i in range (n): # complejidad de  O(n)
+    print("ciclo i")
+
+for j in range (n): # complejidad de O(n)
+    print("ciclo j")
+
+# complejidad O(2 * n) = O(n)
