c622018998b55bef7ccd5cbff182ba389d9dffbc
dneirac <dneirac@unsa.edu.pe>
2021-11-10 11:32:53 -0500

    Agregando comentarios del tiempo computacional

---
+++ EjerciciosClase5/QEjercicios/Q3.cpp
@@ -0,0 +1,19 @@
+//comentarios
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  int i;
+  int n = 10;
+  for(i = 0; i < n; i+=2){ 
+    cout<<"Ejercicio3"<<endl;
+  }
+  /*
+   * Como en este caso el programa avanza de 2 en 2 y iniciamos en 0 2 4 ...  n-2
+   * al final tendriamos O(n/2) = O(n)*/
+
+  return 0;
+}
+
