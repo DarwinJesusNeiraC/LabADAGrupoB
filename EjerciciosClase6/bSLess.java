c7ed6b5b124a57d7cafd533865d4908cdf6b9819
dneirac <dneirac@unsa.edu.pe>
2021-11-16 13:33:12 -0500

    bSLess.java

--- EjerciciosClase6/bSLess.java
+++ EjerciciosClase6/bSLess.java
@@ -8,7 +8,9 @@ public class bSLess{
   public static void main (String[]args){
     // Casos de prueba
     //int [] arr = {6,7,9,15,19,2,3};
-    int [] arr = {9,15,19,1,3,6,7};
+    //int [] arr = {9,15,19,1,3,6,7};
+    //int [] arr = {7, 9, 11, 13, 2, 3, 5}; // error al poner el caso dado en hoja de calculo
+    int [] arr = {7, 9, 11, 13, 1, 2, 3, 5}; // caso dado en hoja de calculo
     //int [] arr = {0,3,6,7,9,15,19};
     System.out.println("menor elemento: " + (binarySearch(arr)));
   }
@@ -24,7 +26,12 @@ public class bSLess{
         high = middle;// low y high
       }
       else if(arr[low] > arr[high]){
-        low = middle;
+        if(arr[low +1 ] < arr[middle]){ // Se agrego un seguro más para el caso de que low sea mayor
+          low = low +1; // Si nuestro elemento de la derecha de middle, es menor que el mismo middle significa que ese debe ser nuestro nuevo valor low
+        }
+        else {
+          low = middle;
+        }
       }
       if(low == high){// cuando se encuentre una coincidencia enter low y high se devolvera el valor
         return arr[middle];
