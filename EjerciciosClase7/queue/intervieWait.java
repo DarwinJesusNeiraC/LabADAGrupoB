50a1d4d269351ea435b3b5fd1a33f917d10bae14
dneirac <dneirac@unsa.edu.pe>
2021-11-18 10:39:39 -0500

    avanze del codigo, se presnte an algunos fallos, interview

---
+++ EjerciciosClase7/queue/intervieWait.java
@@ -0,0 +1,47 @@
+import java.util.Deque;
+import java.util.LinkedList;
+public class intervieWait{
+  public static void main (String[]args){
+    Deque<Integer> myDeque = new LinkedList<Integer>();
+    fill(myDeque);
+    select(myDeque);
+
+    //System.out.println("Cantidad de tiempo: " + select(myDeque));
+    System.out.println("Resultado: " + print(myDeque));
+  }
+
+  private static int select(Deque<Integer> myDeque){
+    int cont = 0;
+    while(true){
+      System.out.println("Primero  : " + myDeque.peek());
+      System.out.println("Final  : " + myDeque.peekLast());
+      if(myDeque.peek() == -1 || myDeque.peekLast() == -1){
+        break;
+      }
+      if(myDeque.peek() < myDeque.peekLast()){
+        cont += myDeque.pollFirst();
+      }
+      else {
+        cont += myDeque.pollLast();
+      }
+    }
+    System.out.println("Cont : " + cont);
+    return cont;
+  }
+  // Este metodo servira para probar más facilmetne el programa
+  private static void fill(Deque<Integer> myDeque){
+    int [] arr = {4,-1,5,2,3};
+    for(int i = 0; i < arr.length; i++){
+      myDeque.addLast(arr[i]);
+    }
+  }
+
+  private static String print(Deque<Integer> myDeque){
+    String retorno = "";
+    for (int i : myDeque) {
+      retorno += i + ", ";
+    }
+    return retorno;
+  }
+}
+
