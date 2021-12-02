ecf36a0e65e544e40147f24c02c64405192b58a8
dneirac <dneirac@unsa.edu.pe>
2021-12-02 15:38:55 -0500

    agragando docuemntacion y comentarios finales

--- EjerciciosClase8/longestSubsequence.java
+++ EjerciciosClase8/longestSubsequence.java
@@ -1,51 +1,49 @@
-import java.util.*;
+/*
+ * @Autor: Darwin Jesus Neira Carrasco
+ * @Email: dneirac@unsa.edu.pe
+ * @Descripcion: Calcula la longitud de la cadena más larga que se puede sacar de un arreglo dado
+ */
+
 public class longestSubsequence{
   public static void main (String[]args){
     //int [] nums = {10,9,2,5,3,7,101,18};
     //int [] nums = {7,7,7,7,7,7,7};
+    // Casos de prueba donde se tuvvo problemas durante el desarrollo
     //int [] nums = {0,1,0,3,2,3};
     //int [] nums = {4,10,4,3,8,9};
     int [] nums = {1,3,6,7,9,4,10,5,6};
     System.out.println("la cadena más larga es de : " + lengthOfLIS(nums));
-
   }
 
   public static int lengthOfLIS(int[] nums){
-    int n = nums.length;
-    int [] cant = new int [n];
+    int n = nums.length; // tamano del arreglo
+    int [] cant = new int [n]; // creacion del arreglo donde se guardara la cadena más larga para cadad item
+    // llenado de casos base, dado que todos empizan con 1
     for (int i = 0; i < cant.length; i++) {
       cant[i] = 1;
     }
 
     for (int i = 0; i < n-1; i++) {
       for (int j = i+1; j < n; j++) {
-        if(nums[i] < nums[j]){
-          cant[j] = Math.max(cant[j], cant[i] + 1);
+        if(nums[i] < nums[j]){ // si el anterior es menor
+          cant[j] = Math.max(cant[j], cant[i] + 1); // en caso el valor de cant[j], sea menor al que tendria con cant[i] + 1, se reemplazara  
         }
         else{
           continue;
         }
       }      
     }
+    // seguimiento
     for (int i : cant) {
       System.out.println(i);
     }
-    int u = cant[0];
+
+    // calculando el mayor de cant
+    int max = cant[0];
     for (int i = 1; i < cant.length; i++) {
-      u = Math.max(u, cant[i]);
+      max = Math.max(max, cant[i]);
     }
-    return u;
-
+    return max;
   }
-  public static void print(int [][] all){
-    for (int i = 0; i < all.length; i++) {
-      System.out.println();
-      for (int j = 0; j < all.length; j++) {
-        System.out.print(all[i][j] + "  ");
-      }
-    }
-    System.out.println();
-  }
-
 }
 
