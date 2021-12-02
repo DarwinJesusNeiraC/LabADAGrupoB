b40ccfd040f04b9faf2e73a3b8dc6c722a229f65
dneirac <dneirac@unsa.edu.pe>
2021-12-01 22:26:05 -0500

    modificaciones para el cumpliento de los valores dados

--- EjerciciosClase8/longestSubsequence.java
+++ EjerciciosClase8/longestSubsequence.java
@@ -4,7 +4,8 @@ public class longestSubsequence{
     //int [] nums = {10,9,2,5,3,7,101,18};
     //int [] nums = {7,7,7,7,7,7,7};
     //int [] nums = {0,1,0,3,2,3};
-    int [] nums = {4,10,4,3,8,9};
+    //int [] nums = {4,10,4,3,8,9};
+    int [] nums = {1,3,6,7,9,4,10,5,6};
     System.out.println("la cadena más larga es de : " + lengthOfLIS(nums));
 
   }
