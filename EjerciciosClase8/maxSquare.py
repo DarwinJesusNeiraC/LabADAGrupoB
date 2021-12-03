c8859bea7b70cedb8782443a7b042ee7a9d7bde3
dneirac <dneirac@unsa.edu.pe>
2021-12-03 15:41:12 -0500

    eliminando prints

--- EjerciciosClase8/maxSquare.py
+++ EjerciciosClase8/maxSquare.py
@@ -29,8 +29,6 @@ def maximalSquare(matrix):
         else:
             for x in range(1,w + 1):
                 for y in range(1,h + 1):
-                    print("x-1 ", x-1)
-                    print("y-1 ", y-1)
                     if matrix[x-1][y-1] == 1:
                         arr[x][y] = min(arr[x-1][y-1],arr[x][y-1],arr[x-1][y]) + 1
                     print("\n-----------------------------------------\n")
