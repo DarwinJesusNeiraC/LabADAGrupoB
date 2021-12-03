995a77ad939d9d0cdbd10962393f44ead4b21977
dneirac <dneirac@unsa.edu.pe>
2021-12-03 15:33:26 -0500

    complentando el docido

--- EjerciciosClase8/maxSquare.py
+++ EjerciciosClase8/maxSquare.py
@@ -1,10 +1,23 @@
 def maximalSquare(matrix):
     # recisicimos un arreglo de tipo str lo convertiremos a int
+    
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
             matrix[x][y] = int(matrix[x][y])
 
-    printMatrix(matrix)
+    # declaracion de un arraglo bidimencional en python
+    w = len(matrix)
+    h = len(matrix[0])
+    print("w: ", w)
+    print(w + 1)
+    print("h: ", h)
+    print(h + 1)
+    # se creo arr, dado que si se modificar matrix, presento un dar error en varios de lso casos de prueba
+    arr = [[0 for x in range(h + 1)] for y in range(w + 1)]
+    print("len arr ", len(arr))
+
+    printMatrix(arr)
+
 
     if len(matrix) == 1 and len(matrix[0]) == 1:
         if matrix[0][0] == 1:
@@ -15,15 +28,19 @@ def maximalSquare(matrix):
         if len(matrix) == 1 or len(matrix[0]) == 1:
             return maxMatrix(matrix)
         else:
-            for x in range(1,len(matrix)):
-                for y in range(1,len(matrix[x])):
-                    matrix[x][y] = min(matrix[x-1][y-1],matrix[x][y-1],matrix[x-1][y]) + 1
+            for x in range(1,w + 1):
+                for y in range(1,h + 1):
+                    print("x-1 ", x-1)
+                    print("y-1 ", y-1)
+                    if matrix[x-1][y-1] == 1:
+                        arr[x][y] = min(arr[x-1][y-1],arr[x][y-1],arr[x-1][y]) + 1
+                    print("\n-----------------------------------------\n")
+                    printMatrix(arr)
                     #if matrix[x-1][y-1] == matrix[x][y-1] and matrix[x-1][y-1] == matrix[x-1][y]:
                     #    matrix[x][y] += 1
                     #else:
                     #    continue
-
-            result = maxMatrix(matrix) 
+            result = maxMatrix(arr) 
             return result * result
 
 # para el seguimiento del programa
@@ -42,12 +59,13 @@ def maxMatrix(matrix):
                 max = matrix[x][y]
     return max
 
+# Casos de prueba
 #matrix = [["1"],["1"],["0"],["0"],["0"],["1"]]
 #matrix = [["1","0","0","1","0"]]
 #matrix = [["0"]]
 #matrix = [["1"]]
 #matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
-matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
-#matrix = [["0","1"],["1","0"]]
+#matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+matrix = [["0","1"],["1","0"]]
 #matrix = [["1","1","1","1","0"],["1","1","1","1","0"],["1","1","1","1","1"],["1","1","1","1","1"],["0","0","1","1","1"]]
 print("Salida: " , maximalSquare(matrix))
