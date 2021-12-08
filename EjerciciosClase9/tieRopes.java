27529fbbd16167b0475268746a6d6e252a855543
dneirac <dneirac@unsa.edu.pe>
2021-12-08 08:23:03 -0500

    tieRopes paso las pruebas de la pagina

--- EjerciciosClase9/tieRopes.java
+++ EjerciciosClase9/tieRopes.java
@@ -1,35 +1,42 @@
 /*
  * @Autor: Darwin Jesus Neira Carrasco
  * @Email: dneirac@unsa.edu.pe
- * @Descripcion: 
+ * @Descripcion: En unir las cuerdas dados por el array A de tal manera que sean mayores o iguales que K,
+ * devolver el maximo de cuerdas posibles
  */
 
 public class tieRopes{
   public static void main (String[]args){
-    int [] A = {1,2,3,4,1,1,3};
-    int K = 4;
+    /*int [] A = {1,2,3,4,1,1,3};
+    //int K = 4;
+    int [] A = {1};
+    int K = 2;*/
+    int [] A = {1,1};
+    int K = 3;
     solution(K, A);
 
   }
 
   public static int solution(int K, int [] A){
     int n = 0;// numeros de cuerdas mayores o iguales a K
-
     for(int i = 0 ; i < A.length ; i++){
-      print(A);
+      print(A);//seguimiento
       if(A[i] >= K){
-        n++;
-        continue;
+        n++;// aumentamos el valor de n
+        continue; // lo saltamos
       }
       else{
-        A[i+1] += A[i];
+        if(i + 1 != A.length){ // verificamos que no nos pasaemos de los limites
+          A[i+1] += A[i]; // establecemos el valor de la nueva cuerda
+        }
       }
-      print(A);
+      print(A); // seguimiento
     }
     System.out.println("salida : " + n);
 
-    return 0;
+    return n;// retornamso el valor
   }
+  // metood de seguimiento
   public static void print(int [] A){
     for (int i : A) {
       System.out.print(i + ", ");
