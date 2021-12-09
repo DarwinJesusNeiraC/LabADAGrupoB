4f3808fbe11f53fd7cfd48dcaa088f9773ebdf99
dneirac <dneirac@unsa.edu.pe>
2021-12-09 16:39:02 -0500

    creacion del archivo para la resolucion de MaxNonoverlappingSegments

---
+++ EjerciciosClase9/maxSegments.java
@@ -0,0 +1,12 @@
+/*
+ * @Autor: Darwin Jesus Neira Carrasco
+ * @Email: dneirac@unsa.edu.pe
+ * @Descripcion: 
+ */
+
+public class maxSegments{
+  public static void main (String[]args){
+
+  }
+}
+
