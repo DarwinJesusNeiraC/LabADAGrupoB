840135df11dabf7f08d87bf5bf00d70dc95f68ee
dneirac <dneirac@unsa.edu.pe>
2021-12-10 23:07:09 -0500

    completando la primera prueba

--- EjerciciosClase9/wateringGrass.py
+++ EjerciciosClase9/wateringGrass.py
@@ -16,25 +16,27 @@ def wateringGrass():
             distance = sqrt(r**2 - halfcuadratic) #distancia de rango de riego
             arr.append((p - distance, p + distance))
 
-    print(arr)
-    arr = sorted(arr)
-    print(arr)
+    print(arr) # seguimiento
+    arr = sorted(arr, reverse= True) # se le asigna dado que  sorted genera un nuevo array
+    print(arr) # seguimiento
     cant = 0 # cantidad de riegadores usados
     length = 0 # la longitud de los que se regara ,tiene que ser mayor a l, para que de esta forma sepamos que se rego todo
-    while(length < l): # recorremos mientras que length sea menor que l
-        for i in range(n): 
-            if (arr[i][0] <= length and arr[i][1] > length):
+    i = 0
+    print("tamaño ", len(arr))
+
+    while(length < l):
+        for i in range(len(arr)):
+            if arr[i][0] <= length and arr[i][1] > length:
                 length = arr[i][1]
-                cant += 1 # aumentamos el numero de riegadores
+                cant += 1
                 break
         if(i == n):
             break
 
     if(length < l):
-        print(-1)
-    else:
-        print(cant)
+        cant = -1
 
+    print(cant)
 
 
 wateringGrass()
