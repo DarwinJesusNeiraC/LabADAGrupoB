3551c29fcde0ea46557e63cc9c22a9f1a7b80fa8
dneirac <dneirac@unsa.edu.pe>
2021-12-10 20:49:34 -0500

    registrando todo el ingreso de datos

--- EjerciciosClase9/wateringGrass.py
+++ EjerciciosClase9/wateringGrass.py
@@ -5,5 +5,17 @@
 
 '''
 
+from  math import sqrt
 def wateringGrass():
+    n , l , w = map(int,input().split())
+    arr = []
+    halfcuadratic = (w/2)**2
+    for i in range(n):
+        p , r = map(int, input().split())
+        if (2 * r) > w:
+            distance = sqrt(r**2 - halfcuadratic)
+            arr.append((p - distance, p + distance))
 
+    print(arr)
+
+wateringGrass()
