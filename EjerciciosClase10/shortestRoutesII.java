5e2963ef7c5f879e5c54d54cace1e325487b3649
dneirac <dneirac@unsa.edu.pe>
2021-12-18 16:35:46 -0500

    eroor de tiempo y salida del valor -1

--- EjerciciosClase10/shortestRoutesII.java
+++ EjerciciosClase10/shortestRoutesII.java
@@ -5,6 +5,7 @@
  */
 import java.util.*;
 public class shortestRoutesII{
+  static final double inf = 1e18;
   public static void main (String[]args){
     Scanner scan = new Scanner(System.in);
     int n = scan.nextInt();
@@ -19,22 +20,22 @@ public class shortestRoutesII{
       arr[a][b] = Math.min(arr[a][b], c);
       arr[b][a] = Math.min(arr[b][a], c);
     }
-    System.out.println();
-    print(arr);
-    for (int k = 1; k < arr.length; k++) {
-      for (int i = 1; i < arr.length; i++) {
-        for (int j = 1; j < arr.length; j++) {
+    //System.out.println();
+    //print(arr);
+    for (int k = 1; k <= n; k++) {
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
           arr[i][j] = Math.min(arr[i][j], (arr[i][k] + arr[k][j]));
         }        
       }
     }
-    System.out.println();
-    print(arr);
+    //System.out.println();
+    //print(arr);
 
     for (int i = 0; i < q; i++) {
       int a = scan.nextInt();
       int b = scan.nextInt();
-      if(arr[a][b] <= 0){
+      if(arr[a][b] >= inf){
         System.out.println("-1");
       }
       else{
@@ -47,7 +48,7 @@ public class shortestRoutesII{
     for (int i = 0; i < arr.length; i++) {
       for (int j = 0; j < arr.length; j++) {
         if(i != j){
-          arr[i][j] = (int) Double.POSITIVE_INFINITY;
+          arr[i][j] = 0x3f;//(int) Double.POSITIVE_INFINITY;
         }
       }      
     }
