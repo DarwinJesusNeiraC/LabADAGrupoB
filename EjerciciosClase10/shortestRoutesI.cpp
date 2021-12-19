c802fbb600934493ac351af93eb1cf5d632e3e2a
dneirac <dneirac@unsa.edu.pe>
2021-12-18 20:20:50 -0500

    agregando comentarios finales, pasando la prueba de la pag

--- EjerciciosClase10/shortestRoutesI.cpp
+++ EjerciciosClase10/shortestRoutesI.cpp
@@ -1,4 +1,8 @@
-//comentarios
+/*
+ * @Autor: Darwin Jesus Neira Carrasco
+ * @Email: dneirac@unsa.edu.pe
+ * @Descripcion: calcular la ruta más corta las distiintas cuidades
+*/
 
 #include<iostream>
 #include <bits/stdc++.h>
@@ -7,6 +11,7 @@ using namespace std;
 typedef long long ll;
 const int maxN = 1e5;
 
+// estructuras usadas
 struct Arista{
   int conection;
   ll peso;
@@ -20,12 +25,14 @@ struct Nodo{
   };
 };
 
+// creacion de variables
 int n, m;
 ll distancias[maxN];
 vector<Arista> conectando [maxN];
 priority_queue<Nodo> data;
 
 int main() {
+  // ingreso de datos inciales
   cin >> n;
   cin >> m;
   for(int i = 0; i < m; i++){
@@ -36,30 +43,33 @@ int main() {
     cin >> c;
     --a;
     --b;
-    conectando[a].push_back({b, c});
+    conectando[a].push_back({b, c}); //agregnado los valores al vector, si tiene más de una salida se ordenaran en el orden que se ingresar
   }
 
-  memset(distancias, 0x3f, sizeof(distancias));
+  // vertice es como una tabla hash
+  memset(distancias, 0x3f, sizeof(distancias)); // llenando el vertice de distancias
   distancias[0] = 0;
   data.push({0,0});
-  while(!data.empty()){
-    ll d = data.top().distance;
-    int u = data.top().indice;
-    data.pop();
 
-    if(d > distancias[u]){
+  while(!data.empty()){ // mientras que el nuestra cola de priority_queue no este vacia
+    ll d = data.top().distance; // extraeremos la distancia
+    int u = data.top().indice; // y su indice
+    data.pop(); // y lo removeremos 
+
+    if(d > distancias[u]){ // si el valor de 'd' es mayor al que esta en el vector de distancias[u] lo salteremos
       continue;
     }
 
-    for(Arista a : conectando[u]){
-      ll x = d + a.peso; 
-      if(distancias[a.conection] > x){
-        distancias[a.conection] = x;
-        data.push({a.conection, x});
+    for(Arista a : conectando[u]){ // recorremos cada artista que este en el vector
+      ll x = d + a.peso;  // el peso de donde viene más del de la arista
+      if(distancias[a.conection] > x){ // el distancia que tiene es mayor al de x
+        distancias[a.conection] = x; // lo reemplazaremos 
+        data.push({a.conection, x});// agremaos el a la  priority_queue
       }
     }
   }
 
+  // impresion de los valores
   for(int i = 0; i < n; i++){
     cout << distancias[i] <<" ";
   }
