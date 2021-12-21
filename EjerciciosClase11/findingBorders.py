0f961c8bbf1b7e471916aff2414c4aa4a6888ff7
dneirac <dneirac@unsa.edu.pe>
2021-12-20 21:37:35 -0500

    se solucionaron lso casos de prueba, pero hay exceso de limite de tiempo

--- EjerciciosClase11/findingBorders.py
+++ EjerciciosClase11/findingBorders.py
@@ -1,19 +1,13 @@
 def findBorders():
     string = input()
-    list = []
-    leght = 0
-    if len(string) % 2 == 0:
-        leght = len(string)
-    else:
-        leght = len(string)-1
+    s = ""   
+    leght = len(string)
+    if len(string) % 2 != 0:
+        leght -= 1
 
     for i in range(1, leght): 
         if string[:i] == string[-i:]:
-            list.append(i)
-    
-    s = ""   
-    for i in list:
-        s += str(i) + " "
+            s += str(i) + " "
     
     print(s)
 
