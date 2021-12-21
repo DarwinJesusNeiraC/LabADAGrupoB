31376125ed241941618d003fb41461ac606ee782
dneirac <dneirac@unsa.edu.pe>
2021-12-20 19:18:37 -0500

    devolviendo la misma palabra y no una cadena

--- EjerciciosClase11/groupAnagrams.py
+++ EjerciciosClase11/groupAnagrams.py
@@ -1,7 +1,11 @@
 
 def groupAnagrams(strs):
-    return sorted(strs)
+    # el metodo sorted ordena y devuelve una cadena,
+    # mediante join agrupomos la palabras en una cadena vacia
+    return "".join(sorted(strs))
 
+
+#los resutlados del a ejercuion fueron los mismos
 print(groupAnagrams("eat"))
 print(groupAnagrams("tea"))
 print(groupAnagrams("ate"))
