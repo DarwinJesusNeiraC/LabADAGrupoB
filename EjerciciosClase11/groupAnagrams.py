27ad6f122a8faa4e387e356b4010a108bab4b12d
dneirac <dneirac@unsa.edu.pe>
2021-12-20 19:58:22 -0500

    probando los casos de prueba de la pagina

--- EjerciciosClase11/groupAnagrams.py
+++ EjerciciosClase11/groupAnagrams.py
@@ -2,12 +2,30 @@
 def groupAnagrams(strs):
     # el metodo sorted ordena y devuelve una cadena,
     # mediante join agrupomos la palabras en una cadena vacia
-    return "".join(sorted(strs))
+    diccionarioWord = {}; # diccionario
+    for i in strs:
+        sortWord =  "".join(sorted(i))
+        if sortWord not in diccionarioWord: # si no estan en el diccionario
+            diccionarioWord[sortWord] = [i] #creamos la variable
+        else:
+            diccionarioWord[sortWord].append(i) # si la existe la variable agegamos elementos
+
+    listadeLista = []
+    for i in diccionarioWord.values():
+        listadeLista.append(i)
+
+    return listadeLista
 
 
 #los resutlados del a ejercuion fueron los mismos
+'''
 print(groupAnagrams("eat"))
 print(groupAnagrams("tea"))
 print(groupAnagrams("ate"))
+'''
+#strs = ["eat","tea","tan","ate","nat","bat"]
+strs = [""]
+#strs = ["a"]
+print(groupAnagrams(strs))
 
 
