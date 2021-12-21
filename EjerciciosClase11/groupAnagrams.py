65194479c73519a83c04f5f2dda94a9e5f725a6b
dneirac <dneirac@unsa.edu.pe>
2021-12-20 20:00:50 -0500

    agregando comentarios finales, pasando el caso de prueba

--- EjerciciosClase11/groupAnagrams.py
+++ EjerciciosClase11/groupAnagrams.py
@@ -1,4 +1,10 @@
 
+'''
+@Autor: Darwin Jesus Neira Carrasco
+@Email: dneirac@unsa.edu.pe
+@Description: Agrupar aquellas palabras que tengan el mismo anaggrama
+'''
+
 def groupAnagrams(strs):
     # el metodo sorted ordena y devuelve una cadena,
     # mediante join agrupomos la palabras en una cadena vacia
@@ -10,13 +16,15 @@ def groupAnagrams(strs):
         else:
             diccionarioWord[sortWord].append(i) # si la existe la variable agegamos elementos
 
+    #lista a devolver
     listadeLista = []
-    for i in diccionarioWord.values():
-        listadeLista.append(i)
+    for i in diccionarioWord.values(): #para cada values en diccionario
+        listadeLista.append(i) #lo metemos en la listadeLista
 
-    return listadeLista
+    return listadeLista #retorno de la lista de lista
 
 
+#CASOS DE PRUEBA
 #los resutlados del a ejercuion fueron los mismos
 '''
 print(groupAnagrams("eat"))
