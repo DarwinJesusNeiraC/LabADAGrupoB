d225b9c3ecb4598accff58ec994fb5a13f169b56
dneirac <dneirac@unsa.edu.pe>
2021-12-22 12:48:35 -0500

    agregando datos basicos para el desarrollo del problema

---
+++ EjerciciosClase11/findingBorders.cpp
@@ -0,0 +1,34 @@
+/*
+ * @Autor: Darwin Jesus Neira Carrasco
+ * @Email: dneirac@unsa.edu.pe
+ * @Descripcion:
+*/
+
+#include<iostream>
+
+using namespace std;
+typedef long long ll;
+
+
+// cantidad maxima a la que puede llegar n
+const int maxN = 1e6; 
+
+// cadnea de string 
+string s;
+// longitud de la cadena
+int n;
+
+
+
+// arreglo de char
+char S [maxN];
+
+int main() {
+  cin >> s;
+  n = s.length(); 
+
+
+
+  return 0;
+}
+
