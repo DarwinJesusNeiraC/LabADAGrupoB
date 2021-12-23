2e8172804d3933b62c113c3da6e1dea3abb26108
dneirac <dneirac@unsa.edu.pe>
2021-12-22 22:17:54 -0500

    pasando las pruebas

--- EjerciciosClase11/findingBorders.cpp
+++ EjerciciosClase11/findingBorders.cpp
@@ -1,7 +1,7 @@
 /*
  * @Autor: Darwin Jesus Neira Carrasco
  * @Email: dneirac@unsa.edu.pe
- * @Descripcion:
+ * @Descripcion: si las coincidencias de los de los sufijos y prefijos es igual, retornar las posiciones donde se da este envento
 */
 
 #include<iostream>
@@ -15,13 +15,13 @@ const ll mp1 = 31;
 const ll mp2 = 37;
 
 // Cantidad maxima a la que puede llegar n
-const int maxN = 1e6; 
+const int maxN = 1e6 + 5; 
 
 // Longitud de la cadena
 int n;
 
 // Al ser varios numeros requerimocs de un modulo
-const ll mod = 1e9;
+const ll mod = 1e9 + 7;
 
 //
 ll potencia1[maxN] , potencia2[maxN], ph1, ph2, sh1, sh2;
@@ -31,6 +31,7 @@ char S [maxN];
 
 int main() {
   //cin >> s;
+  // cadad caracter es ingresado en le array de char
   scanf("%s", S); 
   //n = s.length();  
   /*for(int i = 0; i < s.length(); i++){
@@ -41,25 +42,32 @@ int main() {
   //cout << n <<endl;
 
   potencia2[0] = potencia1[0] = 1;
+  // la cantidad de caracteres 
   for(int i = 1; i < n ; i++){
     potencia1[i] = (potencia1[i - 1] * mp1) % mod;
     potencia2[i] = (potencia2[i - 1] * mp2) % mod;
   }
 
+    // seguimiento
   //cout<< "potencia1" <<endl;
   /*for(int i = 0; i < n; i++){
     cout<< potencia1[i] <<endl;
   }*/
 
   for(int i = 0; i < n-1; i++){
-    int left = (S[i] - 'a' + 1);
-    int rigth = (S[n - i - 1] - 'a' + 1);
+    // se usa 'a' para obtener el en las letras minusculas
+    int left = (S[i] - 'a' + 1); // caracteres de la izquieda
+    // como empezamos desde atras usadmos n
+    int rigth = (S[n - i - 1] - 'a' + 1); // caracteres de la derecha
 
+    // prefijos
     ph1 = (ph1  + left * potencia1[i]) % mod;
     ph2 = (ph2  + left * potencia2[i]) % mod;
+    // sufijos
     sh1 = (sh1 * mp1 + rigth) % mod;
     sh2 = (sh2 * mp2 + rigth) % mod;
 
+    // seguimiento
     //cout << ph1 <<endl;
     //cout << ph2 <<endl;
     //cout << sh1 <<endl;
